ea6a171d07c14c8b912f99113d13be1cc782d0cd
rkkaushik0894 <rkkaushik0894@gmail.com>
2025-12-02 11:49:37 +0530

    change of packages

--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,9 @@
   import { defineConfig } from 'vite';
   import react from '@vitejs/plugin-react-swc';
   import path from 'path';
-  import tailwindcss from '@tailwindcss/vite'
 
   export default defineConfig({
-    plugins: [react(),tailwindcss(),],
+    plugins: [react()],
     resolve: {
       extensions: ['.js', '.jsx', '.ts', '.tsx', '.json'],
       alias: {
